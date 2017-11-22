fc9ccfb25fe1982c320ee9149c90a30609f4300b
yy1193889747 <1193889747@qq.com>
2017-11-22 18:14:03 +0800

    springboot-jpa-thymeleaf

--- springboot-jpa-thymeleaf/src/main/java/com/cy/SpringbootJpaThymeleafApplication.java
+++ springboot-jpa-thymeleaf/src/main/java/com/cy/SpringbootJpaThymeleafApplication.java
@@ -2,11 +2,22 @@ package com.cy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Controller
 @SpringBootApplication
 public class SpringbootJpaThymeleafApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootJpaThymeleafApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpaThymeleafApplication.class, args);
+    }
+
+    @GetMapping(value = "/")
+    public String index() {
+
+        return "redirect:/list";
+
+    }
 }
