37a94d35c524cc07ae0921f27f44c9f5e285debf
yy1193889747 <1193889747@qq.com>
2017-11-22 13:40:14 +0800

    Initial commit

---
+++ springboot-jpa-thymeleaf/src/main/java/com/cy/SpringbootJpaThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.cy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaThymeleafApplication.class, args);
+	}
+}
