23c40003fe08e69f4fca1ed84c671dff26a5a51f
yy1193889747 <1193889747@qq.com>
2017-12-07 10:22:02 +0800

    Scheduled

--- springboot-mybatis-thymeleaf/src/main/java/com/cy/Application.java
+++ springboot-mybatis-thymeleaf/src/main/java/com/cy/Application.java
@@ -1,19 +1,26 @@
 package com.cy;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
-@Controller
+import java.util.Date;
+
+@Log4j2
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
 
-	}
+    }
 
+    @Scheduled(fixedRate = 5 * 1000)
+    public void reportCurrentTime() {
+        log.info("现在时间：{}", new Date());
+    }
 
 }
