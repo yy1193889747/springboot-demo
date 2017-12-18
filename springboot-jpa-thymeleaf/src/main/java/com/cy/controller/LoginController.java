f9d47b433409273b67ffe55cafbae1d0d464fde3
yy1193889747 <1193889747@qq.com>
2017-12-18 08:39:13 +0800

    shiro

--- springboot-jpa-thymeleaf/src/main/java/com/cy/controller/LoginController.java
+++ springboot-jpa-thymeleaf/src/main/java/com/cy/controller/LoginController.java
@@ -5,6 +5,9 @@ import lombok.extern.log4j.Log4j2;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by cy
@@ -20,35 +24,51 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @Log4j2
 public class LoginController {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     /**
      * 登录页面
+     *
      * @return
      */
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login(String message, ModelMap modelMap) {
-        if(!"".equals(message)){
-            modelMap.addAttribute("message",message);
+        if (!"".equals(message)) {
+            modelMap.addAttribute("message", message);
         }
         return "login";
     }
 
     /**
      * 登录动作
+     *
      * @param user
      * @return
      */
-    @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public String login(HttpServletRequest req, UserInfo user, RedirectAttributes attr){
-        if("".equals(user.getUsername()) || "".equals(user.getPassword())){
-            attr.addAttribute("message","用户名或密码不能为空");
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(HttpServletRequest req, UserInfo user, RedirectAttributes attr) {
+        if ("".equals(user.getUsername()) || "".equals(user.getPassword())) {
+            attr.addAttribute("message", "用户名或密码不能为空");
             return "redirect:/login";
         }
         String userName = user.getUsername();
         String pwd = user.getPassword();
-        System.out.println(userName+pwd);
+        System.out.println(userName + pwd);
+        //计数大于5时，设置用户被锁定一小时
+        ValueOperations<String, String> opsForValue = stringRedisTemplate.opsForValue();
+        if ("LOCK".equals(opsForValue.get("SHIRO_IS_LOCK" + userName))) {
+            attr.addAttribute("message", "由于密码输入错误次数大于5次，帐号已经禁止登录！");
+            return "redirect:/login";
+        }
+        if(stringRedisTemplate.hasKey("SHIRO_LOGIN_COUNT" + userName)) {
+            if (Integer.parseInt(opsForValue.get("SHIRO_LOGIN_COUNT" + userName)) >= 5) {
+                opsForValue.set("SHIRO_IS_LOCK" + userName, "LOCK");
+                stringRedisTemplate.expire("SHIRO_IS_LOCK" + userName, 1, TimeUnit.HOURS);
+            }
+        }
         //开始调用shiro验证
-        UsernamePasswordToken token = new UsernamePasswordToken(userName,pwd,"login");
+        UsernamePasswordToken token = new UsernamePasswordToken(userName, pwd, "login");
         //获取当前的subject
         Subject currentUser = SecurityUtils.getSubject();
         System.out.println("asdasd");
@@ -58,33 +78,35 @@ public class LoginController {
             //所以这一步在调用login(token)方法时,它会走到doGetAuthenticationInfo()方法中,具体验证方式详见此方法
             log.info("对用户[" + userName + "]进行登录验证..验证开始");
             currentUser.login(token);
+            opsForValue.set("SHIRO_LOGIN_COUNT"+userName, "0");
             log.info("对用户[" + userName + "]进行登录验证..验证通过");
-        }catch(UnknownAccountException uae){
+        } catch (UnknownAccountException uae) {
             log.info("对用户[" + userName + "]进行登录验证..验证未通过,未知账户");
             attr.addAttribute("message", "用户名不存在");
-        }catch(IncorrectCredentialsException ice){
+        } catch (IncorrectCredentialsException ice) {
+            stringRedisTemplate.opsForValue().increment("SHIRO_LOGIN_COUNT"+userName,1);
             log.info("对用户[" + userName + "]进行登录验证..验证未通过,错误的凭证");
             attr.addAttribute("message", "密码不正确");
-        }catch(LockedAccountException lae){
+        } catch (LockedAccountException lae) {
             log.info("对用户[" + userName + "]进行登录验证..验证未通过,账户已锁定");
             attr.addAttribute("message", "账户已锁定");
-        }catch(ExcessiveAttemptsException eae){
+        } catch (ExcessiveAttemptsException eae) {
             log.info("对用户[" + userName + "]进行登录验证..验证未通过,错误次数过多");
             attr.addAttribute("message", "用户名或密码错误次数过多");
-        }catch(AuthenticationException ae){
+        } catch (AuthenticationException ae) {
             //通过处理Shiro的运行时AuthenticationException就可以控制用户登录失败或密码错误时的情景
             log.info("对用户[" + userName + "]进行登录验证..验证未通过,堆栈轨迹如下");
             ae.printStackTrace();
             attr.addAttribute("message", "用户名或密码不正确");
         }
-        if(req.getParameter("forceLogout") != null) {
+        if (req.getParameter("forceLogout") != null) {
             attr.addAttribute("error", "您已经被管理员强制退出，请重新登录");
             return "redirect:/login";
         }
         //验证是否登录成功
-        if(currentUser.isAuthenticated()){
+        if (currentUser.isAuthenticated()) {
             return "redirect:/index";
-        }else{
+        } else {
             token.clear();
             return "redirect:/login";
         }
