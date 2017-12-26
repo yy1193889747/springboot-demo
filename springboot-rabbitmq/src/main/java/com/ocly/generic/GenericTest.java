8f074a6a0d193b829adfd7242582611f755e3c2c
yy1193889747 <1193889747@qq.com>
2017-12-26 21:36:10 +0800

    thread stop

---
+++ springboot-rabbitmq/src/main/java/com/ocly/generic/GenericTest.java
@@ -0,0 +1,31 @@
+package com.ocly.generic;
+
+/**
+ * Created by cy
+ * 2017/12/26 14:49
+ */
+
+class Hello<T>{
+    private T name;
+
+    public T getName() {
+        return name;
+    }
+
+    public void setName(T name) {
+        this.name = name;
+    }
+
+
+}
+public class GenericTest {
+    public static void main(String[] args) {
+        Hello name = new Hello();
+        name.setName(new Hello());
+        hello(name);
+
+    }
+    public static void hello(Hello h){
+        System.out.println(h.getName());
+    }
+}
